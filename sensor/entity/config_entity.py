f18cf294b15c7ef0849442904ff10836b79b1a6d
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2023-07-29 02:02:10 +0530

    added the data ingestion pipeline

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,5 +1,31 @@
+import os
+from datetime import datetime
+import sys
+from sensor.logger import logging
+from sensor.exception import SensorException
 
-class DataIngestionConfig:...
+FILE_NAME = "sensor.csv"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        self.artifact_dir = os.path.join(os.getcwd(),"artifacts",f"{datetime.now().strftime('%d%m%Y_%H%M%S')}")
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.database_name = "aps"
+        self.collection_name = "sensor"
+        self.data_ingestion_dir = os.path.join(training_pipeline_config,"data_ingestion")
+        self.feature_store_dir = os.path.join(self.data_ingestion_dir,"feature_store_dir")
+        self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
+        self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
+    
+    def to_dict(self)->dict:
+        try:
+            return self.__dict__
+        except Exception as e:
+            raise SensorException(e,sys)
+    
 class DataValidationConfig:...
 class DataTransformationConfig:...
 class ModelTrainerConfig:...
